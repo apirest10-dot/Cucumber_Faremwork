3e73ae9bd6c840f15d706cbf2f67ca6a4dcc9b17
RangaReddy12 <pranga2010@gmail.com>
2026-04-06 09:00:57 +0530

    Method modified

--- src/test/java/testRunner/Demo.java
+++ src/test/java/testRunner/Demo.java
@@ -3,14 +3,14 @@ package testRunner;
 public class Demo {
 public void add()
 {
-	System.out.println("Add");
+	System.out.println("Suuscces  Add");
 }
 public void div()
 {
-	System.out.println("div");
+	System.out.println("Success div");
 }
 public void mul()
 {
-	System.out.println("mul");
+	System.out.println("success mul");
 }
 }

c5e52918fab2e12a60157fae30329c078b4ed0f6
RangaReddy12 <pranga2010@gmail.com>
2026-04-06 08:53:36 +0530

    New Class Added

---
+++ src/test/java/testRunner/Demo.java
@@ -0,0 +1,16 @@
+package testRunner;
+
+public class Demo {
+public void add()
+{
+	System.out.println("Add");
+}
+public void div()
+{
+	System.out.println("div");
+}
+public void mul()
+{
+	System.out.println("mul");
+}
+}
